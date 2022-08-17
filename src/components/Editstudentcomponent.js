19f4d439d0e701e83a79ef5b5a5512bc518dfcce
Sithembiso Maphanga <maphangasthembiso@gmail.com>
2022-08-17 13:34:58 +0200

    Creating Simple React Components

---
+++ src/components/Editstudentcomponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+
+
+export default class EditStudent extends Component {
+  render() {
+    return (
+      <div>
+        <p>React Edit Student Component!</p>
+      </div>
+    );
+  }
+}
