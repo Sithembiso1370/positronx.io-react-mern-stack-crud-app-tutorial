03ee2fe280590be52fcb6d896dd1891ed2c3e8ce
Sithembiso Maphanga <maphangasthembiso@gmail.com>
2022-08-17 14:30:31 +0200

    Using Axios with React to Make HTTP Request

--- src/components/Createstudentcomponent.js
+++ src/components/Createstudentcomponent.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
-
-
 export default class CreateStudent extends Component {
   constructor(props) {
     super(props)
